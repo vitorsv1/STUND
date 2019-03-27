bab4ca41120edb43d2e0d45f21c8851d9c406a0d
IagoMendes <iagorm@al.insper.edu.br>
2019-03-26 22:04:27 -0300

    calculando k individual com alguns erros, falta debug

--- stund.py
+++ stund.py
@@ -4,104 +4,128 @@
 # Software para calcular as forças de tração e compressão em Treliças 
 # 
 # Membros:
-# - Vitor Satyro
 # - Arthur Olga
 # - Guilherme Moraes
 # - Iago Mendes
+# - Vitor Satyro
 # ---------------------------------------------------------------------------------------------------
 
-from functions import *
+import functions
+import math
 
-#Criando objeto para teste - Mudar o nome do input da variavel
-truss = Truss("input.txt",{})
+#Pegando input do arquivo e salvando numa lista ja separado por espaço
+txtInput = open("input.txt", "r").read()
+listInput = txtInput.split("\n")
 
+#Listas que serão salvas no dicionário
+listIncidences = []
+listCoordenates = []
+listElements = []
+listMaterials = []
+listGeoProps = []
+listBCNodes = []
+listLoads = []
 
-#Função que cria o dicionario a partir de uma leitura de arquivo
-def createDict (truss):
+#Dicionário que irá conter todas as informações do Input do arquivo
+dictInput = {}
 
-    #Pegando input do arquivo e salvando numa lista ja separado por espaço
-    txtInput = open(truss.fileName, "r").read()
-    listInput = txtInput.split("\n")       
+for i in range(len(listInput)-1):
+    #Analisa se a palavra começa com *
+    if (listInput[i][0]=='*'):
+        
+        #Ve se logo apos o * a palavra começa com a letra comparada
+        if (listInput[i][1] == 'C'):
+            #Salvo o numero total de coordenadas
+            numCoordenates = int(listInput[i+1])
+            #Salva na lista de coordenadas os valores
+            for j in range(1,numCoordenates+1):
+                listCoordenates.append(listInput[i+j+1])
+            #Salva no dicionário com a chave sendo a letra respectiva e o valor a lista
+            dictInput["Coordinates"] = listCoordenates
+        
+        #O método se repete para todas as letras iniciais        
+        elif (listInput[i][1] == 'E'):
+            numElements = int(listInput[i+1])
+            for j in range(1,numElements+1):
+                listElements.append(listInput[i+j+1])
+            dictInput["Elements"] = listElements
 
-    #Listas que serão salvas no dicionário
-    listIncidences = []
-    listCoordenates = []
-    listElements = []
-    listMaterials = []
-    listGeoProps = []
-    listBCNodes = []
-    listLoads = []
-
-    #Dicionário que irá conter todas as informações do Input do arquivo
-    dictInput = truss.dict
-
-    for i in range(len(listInput)-1):
-        #Analisa se a palavra começa com *
-        if (listInput[i][0]=='*'):
-            
-            #Ve se logo apos o * a palavra começa com a letra comparada
-            if (listInput[i][1] == 'C'):
-                #Salvo o numero total de coordenadas
-                numCoordenates = int(listInput[i+1])
-                #Salva na lista de coordenadas os valores
-                for j in range(1,numCoordenates+1):
-                    listCoordenates.append(listInput[i+j+1])
-                #Salva no dicionário com a chave sendo a letra respectiva e o valor a lista
-                dictInput["Coordinates"] = listCoordenates
-            
-            #O método se repete para todas as letras iniciais        
-            elif (listInput[i][1] == 'E'):
-                numElements = int(listInput[i+1])
-                for j in range(1,numElements+1):
-                    listElements.append(listInput[i+j+1])
-                dictInput["Elements"] = listElements
-
-            elif (listInput[i][1] == 'I'):
-                for j in range(1,numElements+1):
-                    listIncidences.append(listInput[i+j])
-                dictInput["Incidences"] = listIncidences
-            
-            elif (listInput[i][1] == 'M'):
-                numMaterials = int(listInput[i+1])
-                for j in range(1,numMaterials+1):
-                    listMaterials.append(listInput[i+j+1])
-                dictInput["Material"] = listMaterials
-            
-            elif (listInput[i][1] == 'G'):
-                numGeoProps = int(listInput[i+1])
-                for j in range(1,numGeoProps+1):
-                    listGeoProps.append(listInput[i+j+1])
-                dictInput["Geometric Properties"] = listGeoProps
-            
-            elif (listInput[i][1] == 'B'):
-                numBCNodes = int(listInput[i+1])
-                for j in range(1,numBCNodes+1):
-                    listBCNodes.append(listInput[i+j+1])
-                dictInput["Restrictions"] = listBCNodes
-            
-            elif (listInput[i][1] == 'L'):
-                numLoads = int(listInput[i+1])
-                for j in range(1,numLoads+1):
-                    listLoads.append(listInput[i+j+1])
-                dictInput["Loads"] = listLoads
-    
+        elif (listInput[i][1] == 'I'):
+            for j in range(1,numElements+1):
+                listIncidences.append(listInput[i+j])
+            dictInput["Incidences"] = listIncidences #quais nós são ligados pela barra
+        
+        elif (listInput[i][1] == 'M'):
+            numMaterials = int(listInput[i+1])
+            for j in range(1,numMaterials+1):
+                listMaterials.append(listInput[i+j+1])
+            dictInput["Material"] = listMaterials
+        
+        elif (listInput[i][1] == 'G'):
+            numGeoProps = int(listInput[i+1])
+            for j in range(1,numGeoProps+1):
+                listGeoProps.append(listInput[i+j+1])
+            dictInput["Geometric Properties"] = listGeoProps
+        
+        elif (listInput[i][1] == 'B'):
+            numBCNodes = int(listInput[i+1])
+            for j in range(1,numBCNodes+1):
+                listBCNodes.append(listInput[i+j+1])
+            dictInput["Restrictions"] = listBCNodes
+        
+        elif (listInput[i][1] == 'L'):
+            numLoads = int(listInput[i+1])
+            for j in range(1,numLoads+1):
+                listLoads.append(listInput[i+j+1])
+            dictInput["Loads"] = listLoads
+#print(dictInput)
             
 ##############################################################################################################
 
-if __name__ == '__main__':
-    m = [[1.59, -0.4, -0.54], [-0.4, 1.7, 0.4], [-0.54, 0.4, 0.54]]
-    m = [[i * 1e8 for i in j] for j in m]
+# Passo 1: Pegar os dois pontos da incidencia, uma incidencia é igual a um K
+# Passo 2: Com as Coord do ponto, calcular sen e cos e distancia
+# Passo 3: Montar a matriz de cada K e preencher numa lista_K
+# Passo 4: Montar o K Global (Numero de pontos * 2)
+# Passo 5: Cortar as restrições na matriz K Global
 
-    F = [0, 150, -100]
+lista_k = []
+material = 0
+for incidencia in listIncidences:
+    incidencia = incidencia.split(' ')
+    ponto1 = incidencia[1] 
+    ponto2 = incidencia[2]
 
-    r1, r2 = gauss_seidel(50, 0.000001, m, F)
+    for ponto in listCoordenates:
+        ponto = ponto.split(' ')
+        if ponto[0] == ponto1:
+            coordenada1 = [float(ponto[1]), float(ponto[2])]
+        elif ponto[0] == ponto2:
+            coordenada2 = [float(ponto[1]), float(ponto[2])]
 
-    print(f'\nResultado: {r1}\nIterações: {r2}')
+    distancia = math.sqrt((coordenada2[0] - coordenada1[0])**2 + (coordenada2[1] - coordenada1[1])**2)
+    cos = coordenada1[0] - coordenada2[0] / distancia   #cos = coordenada2[0] - coordenada1[0] / distancia
+    sen = coordenada1[1] - coordenada2[1] / distancia   #sen = coordenada2[1] - coordenada1[1] / distancia
+
+    modulo_elasticidade = float(listMaterials[material].split(' ')[0])
+    area = float(listGeoProps[material].split(' ')[0])
+
+    c = (cos**2) * modulo_elasticidade * area / distancia
+    cs = (cos*sen) * modulo_elasticidade * area / distancia
+    s = (sen**2) * modulo_elasticidade * area / distancia
+
+    lista_ki = [[c, cs, -c, -cs],
+                [cs, s, -cs, -s],
+                [-c, cs, c, cs],
+                [-cs, -s, cs, s]]
+    print(lista_ki)
+    lista_k.append(lista_ki)
+    material += 1 
 
 ##############################################################################################################  
-
 #Escrevendo arquivo de saída
+
 output_file = open("output.txt", "w")
+
 output_file.write("*DISPLACEMENTS\n")
 output_file.write("resultados lalala\n\n")
 output_file.write("*ELEMENT_STRAINS\n")
@@ -110,5 +134,6 @@ output_file.write("*ELEMENT_STRESSES\n")
 output_file.write("resultados lalala\n\n")
 output_file.write("*REACTION_FORCES\n")
 output_file.write("resultados lalala\n\n")
+
 
 output_file.close()
